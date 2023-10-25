bee187ce95084ba77b44cbaa34224aaf3293a5c8
Sparklel Malone <sparklelmalone@gmail.com>
2023-10-24 21:38:48 -0400

    sample code to get file from google drive

--- Week 2-Meterological Data.R
+++ Week 2-Meterological Data.R
@@ -1,1 +1,12 @@
 # Welcome to week 2: 
+
+# install.packages("googledrive")
+library(googledrive)
+
+temp <- tempfile(fileext = ".zip", tmpdir =getwd())
+dl <- drive_download(
+  as_id("1VLqqz3l8qNTdJFwVwVLjZaa9M4cdx7U6WhvNCLQKStg"), path = temp, overwrite = TRUE)
+
+out <- unzip(temp, exdir = tempdir())
+
+met.data <- read.csv(out[2], sep = ";")
